844a7d0dad71d18d2671a91a9510d0d7ae3017f2
minimimin <139435516+minimimin@users.noreply.github.com>
2024-01-08 22:13:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.88 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,7 @@
-from collections import defaultdict
-
 def solution(participant, completion):
-    participant_dic = defaultdict(int)
-    for i in participant:
-        participant_dic[i] += 1
-    for j in completion:
-        participant_dic[j] -= 1
-        if participant_dic[j] == 0:
-            del(participant_dic[j])
-    return list(participant_dic.keys())[0]
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
