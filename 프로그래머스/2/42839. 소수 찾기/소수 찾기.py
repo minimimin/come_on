f703bb8fb1125594ac773ef33aff42a44fb212a1
minimimin <139435516+minimimin@users.noreply.github.com>
2024-04-23 21:47:09 +0900

    [level 2] Title: 소수 찾기, Time: 0.90 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,46 @@
+answer = 0
+def findSosu(num):
+    global answer
+    for i in range(1, num):
+        if i != 1 and num % i == 0:
+            return 0
+    if num>1:
+        answer +=1
+    return 1
+
+def solution(numbers):
+    global answer
+    # johap = set()
+    # for i in range(len(numbers)):
+    #     for j in range(i, len(numbers)):
+    #         num = int(numbers[i]+numbers[j])
+    # num = int(numbers)
+    n = len(numbers)
+    visited=[0 for _ in range(n)]
+    check = set()
+    def sunyeol(result,n):
+        temp2 = int(result)
+        if temp2 != 0:
+            if not temp2 in check:
+                findSosu(temp2)
+                check.add(temp2)
+        for i in range(n):
+            if visited[i] == 0:
+                temp3 = ""
+                temp3 += result
+                temp3 += numbers[i]
+                visited[i] =1
+                sunyeol(temp3,n)
+                visited[i] =0
+        
+        
+    for i in range(n):
+        temp =""
+        temp += numbers[i]
+        visited[i]=1
+        sunyeol(temp,n)
+        visited[i]=0
+    
+    
+    
+    return answer
