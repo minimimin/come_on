f7597fee7c990a32f472acbe3b92cc385b3f8b85
minimimin <139435516+minimimin@users.noreply.github.com>
2024-06-09 14:55:06 +0900

    [level 2] Title: 피로도, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,34 +1,19 @@
-# dungeons 길이로 순열 구해서 다 해보기!
-# 최소 필요 피로도가 더 크면 리턴하기
-# 카운트 해놓은 거 담을 리스트도 필요하다
-# 
 from itertools import permutations
+from functools import partial
 
-# def check_all(index_list):
-#     global k, dungeons
-#     now_k = k
-#     dungeon_count = 0
-#     for index in index_list:
-#         if now_k < dungeons[index][0]:
-#             return dungeon_count
-#         else:
-#             now_k -= dungeons[index][1]
-#             dungeon_count += 1
-#     return dungeon_count
+def check_all(index_list, k, dungeons):
+    now_k = k
+    dungeon_count = 0
+    for index in index_list:
+        if now_k < dungeons[index][0]:
+            return dungeon_count
+        else:
+            now_k -= dungeons[index][1]
+            dungeon_count += 1
+    return dungeon_count
 
 def solution(k, dungeons):
     permu = permutations([_ for _ in range(len(dungeons))], len(dungeons))
-    # answer = list(map(check_all, permu))
-    answer = []
-    for index_list in permu:
-        now_k = k
-        dungeon_count = 0
-        for index in index_list:
-            if now_k < dungeons[index][0]:
-                answer.append(dungeon_count)
-                break
-            else:
-                now_k -= dungeons[index][1]
-                dungeon_count += 1
-        answer.append(dungeon_count)
+    check_all_partial = partial(check_all, k=k, dungeons=dungeons)
+    answer = list(map(check_all_partial, permu))
     return max(answer)
