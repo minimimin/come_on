1562ae5baf968cbfda052b562d112cb89d4f8370
minimimin <139435516+minimimin@users.noreply.github.com>
2024-06-10 21:33:25 +0900

    [level 1] Title: 폰켓몬, Time: 1.38 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,15 @@
+# 전체 나와있는 것들 중에서 반만큼만 가져갈 수 있다. (len(num)//2 만큼만! => 어차피 짝수로 주어진다고 함)
+
+def solution(nums):
+    temp_dic = {}
+    length = len(nums)
+    for i in nums:
+        if i in temp_dic:
+        # if temp_dic[i]:
+            temp_dic[i] += 1
+        else:
+            temp_dic[i] = 1
+    if len(temp_dic) > (length//2):
+        return length//2
+    else:
+        return len(temp_dic)
