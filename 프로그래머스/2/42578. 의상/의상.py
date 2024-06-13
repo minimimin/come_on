afbf3454e539c5b543efb22938ff056985161d79
minimimin <139435516+minimimin@users.noreply.github.com>
2024-06-13 20:23:04 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -2,15 +2,13 @@ from collections import defaultdict
 
 def solution(clothes):
     # 의상 종류에 따른 의상 이름 리스트 딕셔너리
-    dic_clothes = defaultdict(list)
+    dic_clothes = defaultdict(int)
     for name, category in clothes:
-        dic_clothes[category].append(name)
+        dic_clothes[category] += 1
 
     # 종류별 전체 옷이 몇 벌씩 있는지
-    how_many_clothes = [len(dic_clothes[clothes_list]) for clothes_list in dic_clothes]
-    
     # 경우의 수에 따라 (각 요소+1)한걸 전부 곱한 후 -1 해주면 됨
     answer = 1
-    for count in how_many_clothes:
+    for count in dic_clothes.values():
         answer *= (count + 1)
     return (answer - 1)
