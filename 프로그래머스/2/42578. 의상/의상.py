9f1ff6a133540c70009d5c7d8c66d953b6242a2b
minimimin <139435516+minimimin@users.noreply.github.com>
2024-06-13 12:36:56 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -8,6 +8,7 @@ def solution(clothes):
 
     # 종류별 전체 옷이 몇 벌씩 있는지
     how_many_clothes = [len(dic_clothes[clothes_list]) for clothes_list in dic_clothes]
+    
     # 경우의 수에 따라 (각 요소+1)한걸 전부 곱한 후 -1 해주면 됨
     answer = 1
     for count in how_many_clothes:
