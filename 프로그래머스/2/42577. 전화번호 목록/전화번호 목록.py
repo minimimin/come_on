dcaa99178398c14458a23e93497f556731d9281a
minimimin <139435516+minimimin@users.noreply.github.com>
2024-06-14 19:00:15 +0900

    [level 2] Title: 전화번호 목록, Time: 86.17 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,14 @@
 def solution(phone_book):
-    temp_books = set(phone_book)
-    for num in phone_book:
-        length = len(num)
-        for index in range(length):
-            if num[0:index] in temp_books:
-                return False
-    return True
+# 문자열 특성 이용하기!!
+    temp_book = sorted(phone_book)
+    # 전체 전화번호 수
+    length = len(phone_book)
+    # 전부 확인해보기
+    for i in range(length-1):
+    # 기준이 되는 전화번호 총 글자 수
+        i_len = len(temp_book[i])
+        # 전부 뒤 전화번호부랑 접두사로 확인해보기
+        if temp_book[i] in temp_book[i+1][:i_len]:
+            return False
+    else:
+        return True
