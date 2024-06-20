5de45bf3591fb912224145ff9e536bf400be4374
minimimin <139435516+minimimin@users.noreply.github.com>
2024-06-20 17:40:23 +0900

    [level 2] Title: 프로세스, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,17 +1,13 @@
+from collections import deque
+
 def solution(priorities, location):
-    answer = 0
-    find_num = (priorities[location], location)
-    im_que = []
-    im_max_list = sorted(priorities, reverse=True)
-    # im_max_list.reverse()
-    for i in range(len(priorities)):
-        im_que.append((priorities[i], i))
-    while im_que:
-        if im_que[0][0] == im_max_list[0]:
-            answer += 1
-            if im_que[0] == find_num:
-                return answer
-            im_max_list.pop(0)
-            im_que.pop(0)
+    pri_que = deque([(priorities[i], i) for i in range(len(priorities))])
+    count = 0
+    while pri_que:
+        current = pri_que.popleft()
+        if any(current[0] < q[0] for q in pri_que):
+            pri_que.append(current)
         else:
-            im_que.append(im_que.pop(0))
+            count += 1
+            if current[1] == location:
+                return count
