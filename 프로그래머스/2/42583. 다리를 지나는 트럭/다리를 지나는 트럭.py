57d6530da6f3ef074fa2506e59a7dd676f2ea8b7
minimimin <139435516+minimimin@users.noreply.github.com>
2024-06-25 11:46:18 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,67 +1,21 @@
-import collections
-
-DUMMY_TRUCK = 0
-
-
-class Bridge(object):
-
-    def __init__(self, length, weight):
-        self._max_length = length
-        self._max_weight = weight
-        self._queue = collections.deque()
-        self._current_weight = 0
-
-    def push(self, truck):
-        next_weight = self._current_weight + truck
-        if next_weight <= self._max_weight and len(self._queue) < self._max_length:
-            self._queue.append(truck)
-            self._current_weight = next_weight
-            return True
-        else:
-            return False
-
-    def pop(self):
-        item = self._queue.popleft()
-        self._current_weight -= item
-        return item
-
-    def __len__(self):
-        return len(self._queue)
-
-    def __repr__(self):
-        return 'Bridge({}/{} : [{}])'.format(self._current_weight, self._max_weight, list(self._queue))
-
+from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    bridge = Bridge(bridge_length, weight)
-    trucks = collections.deque(w for w in truck_weights)
+    bridge = deque(0 for _ in range(bridge_length))
+    total_weight = 0
+    step = 0
+    truck_weights.reverse()
 
-    for _ in range(bridge_length):
-        bridge.push(DUMMY_TRUCK)
-
-    count = 0
-    while trucks:
-        bridge.pop()
-
-        if bridge.push(trucks[0]):
-            trucks.popleft()
+    while truck_weights:
+        total_weight -= bridge.popleft()
+        if total_weight + truck_weights[-1] > weight:
+            bridge.append(0)
         else:
-            bridge.push(DUMMY_TRUCK)
+            truck = truck_weights.pop()
+            bridge.append(truck)
+            total_weight += truck
+        step += 1
 
-        count += 1
+    step += bridge_length
 
-    while bridge:
-        bridge.pop()
-        count += 1
-
-    return count
-
-
-def main():
-    print(solution(2, 10, [7, 4, 5, 6]), 8)
-    print(solution(100, 100, [10]), 101)
-    print(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]), 110)
-
-
-if __name__ == '__main__':
-    main()
+    return step
