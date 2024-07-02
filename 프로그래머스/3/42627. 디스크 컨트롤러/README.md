cc0d00bddedcfb0b188e320de749de0d76e019dc
minimimin <139435516+minimimin@users.noreply.github.com>
2024-07-02 10:47:33 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 30일 15:48:44
+2024년 07월 02일 10:47:30
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,24 +1,21 @@
-import heapq
-
 def solution(jobs):
-    answer = 0
-    time = 0
-    jobs_cnt = len(jobs)
-    heapq.heapify(jobs)
-    
-    temp = []
-    while jobs or temp:
-        while jobs and jobs[0][0] <= time:
-            waiting_time, working_time = heapq.heappop(jobs)
-            heapq.heappush(temp, [working_time, waiting_time])
-        if temp:
-            work, wait = heapq.heappop(temp)
-            time += work
-            start_time = wait
-            answer += time - start_time
-        elif jobs:
-            waiting_time, working_time = heapq.heappop(jobs)
-            time = waiting_time + working_time
-            start_time = waiting_time 
-            answer += working_time
-    return answer//jobs_cnt
+    answer = 0  # 작업요청부터 종료까지 걸린 총시간
+    # 소요 시간 오름차순 정렬
+    n = len(jobs)
+    sorted_list = sorted(jobs, key=lambda x: x[1])
+
+    start = 0 #현재시간
+    while sorted_list:
+        for i in range(len(sorted_list)):
+            # 작업 시점이 start보다 작거나같으면 작업시작하기
+            if sorted_list[i][0] <= start:
+                start += sorted_list[i][1]
+                answer += start - sorted_list[i][0]
+                sorted_list.pop(i)
+                break
+            
+
+        else: # 반복문을 모두 돌았을 경우에도 시작하는 시간이없다면 시간을 1증가
+            start += 1
+
+    return answer // n
