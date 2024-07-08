890225f890c04926414b3720eda07bf322af415e
minimimin <139435516+minimimin@users.noreply.github.com>
2024-07-09 02:47:18 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,8 +1,9 @@
-# 갈색은 (가로 + 세로 - 2)*2
-# 노란색은 (가로-2)*(세로-2)
+# 갈색은 (가로 + 세로-2)*2
+# 노란색은 (가로-2)*(세로-2) = 약수조합으로 구해도 된다
 
 def solution(brown, yellow):
-    for i in range(brown//2-1, 0, -1):
-        for j in range(brown//2-1, 0, -1):
-            if (i + j - 2) * 2 == brown and (i-2)*(j-2) == yellow:
+    for i in range(3, brown//2+1):
+        if yellow % (i-2) == 0:
+            j = (yellow // (i-2)) + 2
+            if (i+j-2)*2 == brown:
                 return [i, j] if i >= j else [j, i]
