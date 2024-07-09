9b5f057cdfa2c4334c59312ce8b10592ee35e3d0
minimimin <139435516+minimimin@users.noreply.github.com>
2024-07-10 01:46:07 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,19 +1,18 @@
 from itertools import permutations
-from functools import partial
 
-def check_all(index_list, k, dungeons):
-    now_k = k
-    dungeon_count = 0
-    for index in index_list:
-        if now_k < dungeons[index][0]:
-            return dungeon_count
+def go_dungeon(order, fatigue, dungeons):
+    defeat_dungeon = 0
+    for sunseo in order:
+        if fatigue >= dungeons[sunseo][0]:
+            fatigue -= dungeons[sunseo][1]
+            defeat_dungeon += 1
         else:
-            now_k -= dungeons[index][1]
-            dungeon_count += 1
-    return dungeon_count
+            return defeat_dungeon
+    return defeat_dungeon
+    
 
 def solution(k, dungeons):
-    permu = permutations([_ for _ in range(len(dungeons))], len(dungeons))
-    check_all_partial = partial(check_all, k=k, dungeons=dungeons)
-    answer = list(map(check_all_partial, permu))
-    return max(answer)
+    dungeon_cnt = len(dungeons)
+    can_go_sunyeol = permutations([i for i in range(dungeon_cnt)], dungeon_cnt)
+    answer = max(list(map(lambda order : go_dungeon(order, fatigue=k, dungeons=dungeons), can_go_sunyeol)))
+    return answer if answer > 0 else 0
