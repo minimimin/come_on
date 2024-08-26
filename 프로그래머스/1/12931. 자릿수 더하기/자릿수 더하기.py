ff823f8390666b406dd6ab7762782c107f178007
minimimin <139435516+minimimin@users.noreply.github.com>
2024-08-26 22:03:44 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,5 +1,8 @@
 def solution(n):
-    answer = 0
-    for i in str(n):
-        answer += int(i)
-    return answer
+    # answer = 0
+    # for i in str(n):
+    #     answer += int(i)
+    # return answer
+    if n < 10:
+        return n
+    return n % 10 + solution(n // 10)
