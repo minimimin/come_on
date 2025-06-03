9186c0806d35e67d9cfeadbbbb6a4f4997563a1f
minimimin <139435516+minimimin@users.noreply.github.com>
2025-06-03 17:25:09 +0900

    [level 0] Title: [PCCE 기출문제] 3번 / 수 나누기, Time: 289.06 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/340205. ［PCCE 기출문제］ 3번 ／ 수 나누기/［PCCE 기출문제］ 3번 ／ 수 나누기.java
+++ 프로그래머스/0/340205. ［PCCE 기출문제］ 3번 ／ 수 나누기/［PCCE 기출문제］ 3번 ／ 수 나누기.java
@@ -6,7 +6,7 @@ public class Solution {
         int number = sc.nextInt();
         int answer = 0;
         
-        for(int i=0; i<10; i++){
+        for(int i=0; number>0; i++){
             answer += number % 100;
             number /= 100;
         }
