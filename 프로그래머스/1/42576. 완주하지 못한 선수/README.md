d6742b4a6c7305e0b9a57a2e8cf994f1416ac48b
minimimin <139435516+minimimin@users.noreply.github.com>
2025-08-07 20:08:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.36 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 26.2 MB, 시간: 68.88 ms
+메모리: 33.1 MB, 시간: 48.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 22:13:35
+2025년 08월 07일 20:08:42
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,26 @@
+# def solution(participant, completion):
+#     participant.sort()
+#     completion.sort()
+#     for i in range(len(completion)):
+#         if participant[i] != completion[i]:
+#             return participant[i]
+#     return participant[-1]
+
+# def solution(participant, completion):
+#     for i in participant:
+#         if i not in completion:
+#             return i
+        
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-    return participant[-1]
+    dict_part = {}
+    for finisher in completion:
+        if finisher in dict_part:
+            dict_part[finisher] += 1
+        else:
+            dict_part[finisher] = 1
+    for name in participant:
+        if name in dict_part and dict_part[name]:
+            dict_part[name] -= 1
+        else:
+            return name
+    
