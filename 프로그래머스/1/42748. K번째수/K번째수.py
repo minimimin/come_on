18fe4b977055c8807492698949c286ed7c500327
minimimin <139435516+minimimin@users.noreply.github.com>
2025-09-05 01:15:21 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,13 +1,5 @@
-# def solution(array, commands):
-#     answer = []
-#     for i, j, k in commands:
-#         temp = sorted(array[i-1:j])
-#         answer.append(temp[k-1])
-#     return answer
-
 def solution(array, commands):
     answer = []
     for i, j, k in commands:
-        temp = sorted(array[i-1:j])
-        answer.append(temp[k-1])
+        answer.append(sorted(array[i-1:j])[k-1])
     return answer
