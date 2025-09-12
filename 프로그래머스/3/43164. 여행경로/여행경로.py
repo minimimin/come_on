fea143db0fdcafab7d198dd7268d32b1385aaf02
minimimin <139435516+minimimin@users.noreply.github.com>
2025-09-12 20:24:32 +0900

    [level 3] Title: 여행경로, Time: 0.06 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,22 +1,21 @@
-def find_route(answer, tickets, visited, start, ticket_count):
-    # 모든 항공권을 사용한 경우
-    if len(answer) == ticket_count + 1:
-        return list(answer)
-    
-    # 항공권을 알파벳 순으로 탐색
-    for i in range(len(tickets)):
-        if tickets[i][0] == answer[-1] and not visited[i]:
-            visited[i] = True
-            answer.append(tickets[i][1])
-            result = find_route(answer, tickets, visited, i, ticket_count)
-            if result:
-                return result
-            visited[i] = False
-            answer.pop()
-    
-    return None
-
 def solution(tickets):
-    tickets.sort()  # 알파벳 순으로 정렬
-    visited = [False] * len(tickets)
-    return find_route(['ICN'], tickets, visited, 0, len(tickets))
+    visited = [0 for _ in range(len(tickets))]
+    airports = ["ICN"]
+    tickets.sort()
+
+    def DFS():
+        if len(airports) > len(tickets) and all(visited):
+        # if len(airports) > len(tickets):
+            print(airports)
+            return airports
+        for ticket_num in range(len(tickets)):
+            if tickets[ticket_num][0] == airports[-1] and not visited[ticket_num]:
+                visited[ticket_num] = 1
+                airports.append(tickets[ticket_num][1])
+                if DFS():
+                    return airports
+                airports.pop()
+                visited[ticket_num] = 0
+
+    if DFS():
+        return airports
