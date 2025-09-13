6019148ae5580af23b5c68d482377e106b461ccc
minimimin <139435516+minimimin@users.noreply.github.com>
2025-09-13 13:17:29 +0900

    [level 3] Title: 네트워크, Time: 0.36 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,14 +1,19 @@
-def dfs(k, n, visited, computers):
-    visited[k] = 1
-    for i in range(n):
-        if not visited[i] and computers[k][i]:
-            dfs(i, n, visited, computers)
-            
 def solution(n, computers):
-    visited = [0] * n
-    count = 0
+    visited = [0]*n
+    count = [0]
+    
+    def dfs(i):
+        for j in range(n):
+            if not visited[j] and computers[i][j]:
+                visited[j] = 1
+                dfs(j)
+
+        # else:
+        #     count[0] += 1
+                
     for i in range(n):
         if not visited[i]:
-            count += 1
-            dfs(i, n, visited, computers)
-    return count
+            visited[i] = 1
+            dfs(i)
+            count[0] += 1
+    return count[0]
