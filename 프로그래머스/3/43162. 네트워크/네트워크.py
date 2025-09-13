56cd346ac6f4271f7af886d06eee5b82fe98b0e9
minimimin <139435516+minimimin@users.noreply.github.com>
2025-09-13 14:52:40 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,20 @@
+from collections import deque
+
 def solution(n, computers):
     visited = [0]*n
-    count = [0]
-    
-    def dfs(i):
-        for j in range(n):
-            if not visited[j] and computers[i][j]:
-                visited[j] = 1
-                dfs(j)
+    count = 0
+    check = deque([])
 
-        # else:
-        #     count[0] += 1
+    for network in range(n):
+        if not visited[network]:
+            visited[network] = 1
+            check.append(network)
+            while check:
+                now_net = check.popleft()
+                for next_net in range(n):
+                    if not visited[next_net] and computers[now_net][next_net]:
+                        visited[next_net] = 1
+                        check.append(next_net)
+            count += 1
                 
-    for i in range(n):
-        if not visited[i]:
-            visited[i] = 1
-            dfs(i)
-            count[0] += 1
-    return count[0]
+    return count
