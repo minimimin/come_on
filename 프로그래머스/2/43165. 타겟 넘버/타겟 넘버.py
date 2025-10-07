6384de6e72e491e8909c1dc2463f3fb34a5d8858
minimimin <139435516+minimimin@users.noreply.github.com>
2025-10-07 21:02:12 +0900

    [level 2] Title: 타겟 넘버, Time: 461.02 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,10 @@
-from itertools import product
-
 def solution(numbers, target):
-    count = 0
-    for cal_list in product(['+','-'], repeat=len(numbers)):
-        temp_sum = 0
-        for i in range(len(numbers)):
-            if cal_list[i] == '+':
-                temp_sum += numbers[i]
-            else:
-                temp_sum -= numbers[i]
-        if temp_sum == target:
-            count += 1
-    return count
+    def dfs(now_idx, now_sum):
+        if now_idx == len(numbers) and now_sum == target:
+            return 1
+        elif now_idx >= len(numbers):
+            return 0
+            
+        return dfs(now_idx+1, now_sum + numbers[now_idx]) + dfs(now_idx+1, now_sum - numbers[now_idx])
+        
+    return dfs(1, 0 + numbers[0]) + dfs(1, 0 - numbers[0])
