d6abf9bc5b5caffabb86cbbb855f6f9c809b77f1
minimimin <139435516+minimimin@users.noreply.github.com>
2025-11-08 03:26:26 +0900

    [level 1] Title: 실패율, Time: 13.73 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,22 +1,13 @@
+from collections import Counter
+
 def solution(N, stages):
-    # 스테이지별 실패한 사람 수
-    fail_person = [0]*(N+1)
-    for person in stages:
-        if person > N:
+    answer = {}
+    peopleCnt = len(stages)
+    stageFailCnt = Counter(stages)
+    for stage in range(1,N+1):
+        if stage not in stageFailCnt:
+            answer[stage] = 0
             continue
-        fail_person[person] += 1
-
-    # 스테이지별 실패율
-    mans_cnt = len(stages)
-    # lambda식 활용을 위해 딕셔너리로 만들기
-    fail_percent = {}
-    for stage in range(N):
-        if fail_person[stage+1] == 0:
-            fail_percent[stage+1] = 0
-            continue
-        fail_percent[stage+1] = fail_person[stage+1]/mans_cnt
-        mans_cnt -= fail_person[stage+1]
-
-    # 실패율이 높은 스테이지순으로 반환
-    # lambda식 활용 <= 딕셔너리 사용하기!!
-    return sorted(fail_percent, key=lambda x : fail_percent[x], reverse=True)
+        answer[stage] = stageFailCnt[stage]/peopleCnt
+        peopleCnt -= stageFailCnt[stage]
+    return sorted(answer, key=lambda x : answer[x], reverse=True)
