b8dc057a348a41bcc3a38417a74ae3400799f88e
minimimin <139435516+minimimin@users.noreply.github.com>
2025-11-09 00:21:33 +0900

    [level 1] Title: 실패율, Time: 19.78 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,13 +1,40 @@
-from collections import Counter
-
 def solution(N, stages):
-    answer = {}
-    peopleCnt = len(stages)
-    stageFailCnt = Counter(stages)
-    for stage in range(1,N+1):
-        if stage not in stageFailCnt:
-            answer[stage] = 0
+    # 스테이지별 실패한 사람 수
+    fail_person = [0]*(N+1)
+    for person in stages:
+        if person > N:
             continue
-        answer[stage] = stageFailCnt[stage]/peopleCnt
-        peopleCnt -= stageFailCnt[stage]
-    return sorted(answer, key=lambda x : answer[x], reverse=True)
+        fail_person[person] += 1
+
+    # 스테이지별 실패율
+    mans_cnt = len(stages)
+    fail_percent = [0]*(N+1)
+    for stage in range(N):
+        if fail_person[stage+1] == 0: 
+            fail_percent[stage+1] = 0
+            continue
+        fail_percent[stage+1] = fail_person[stage+1]/mans_cnt
+        mans_cnt -= fail_person[stage+1]
+    # lambda식 활용을 위해 딕셔너리로 만들기
+    # fail_percent = {}
+    # for stage in range(N):
+    #     if fail_person[stage+1] == 0:
+    #         fail_percent[stage+1] = 0
+    #         continue
+    #     fail_percent[stage+1] = fail_person[stage+1]/mans_cnt
+    #     mans_cnt -= fail_person[stage+1]
+
+    # 실패율이 높은 스테이지순으로 반환
+    stage_per = []
+    for idx, per in enumerate(fail_percent):
+        if idx == 0:
+            continue
+        stage_per.append((per,idx))
+    # print(stage_per)
+    # stage_per.sort(reverse=True)
+    # print(stage_per)
+    # answer = [stg for per, stg in stage_per]
+    stage_per.sort(key=lambda x: x[0], reverse=True)
+    return [stg for per, stg in stage_per]
+    # lambda식 활용 <= 딕셔너리 사용하기!!
+    # return sorted(fail_percent, key=lambda x : fail_percent[x], reverse=True)
