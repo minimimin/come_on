cb271fcecef4b667fb3c3c1c383b355c638d9d7f
minimimin <139435516+minimimin@users.noreply.github.com>
2025-11-25 20:15:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.00 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,26 +1,9 @@
-# def solution(participant, completion):
-#     participant.sort()
-#     completion.sort()
-#     for i in range(len(completion)):
-#         if participant[i] != completion[i]:
-#             return participant[i]
-#     return participant[-1]
+from collections import Counter
 
-# def solution(participant, completion):
-#     for i in participant:
-#         if i not in completion:
-#             return i
-        
 def solution(participant, completion):
-    dict_part = {}
-    for finisher in completion:
-        if finisher in dict_part:
-            dict_part[finisher] += 1
-        else:
-            dict_part[finisher] = 1
-    for name in participant:
-        if name in dict_part and dict_part[name]:
-            dict_part[name] -= 1
-        else:
-            return name
-    
+    participant = Counter(participant)
+    for person in completion:
+        participant[person] -= 1
+    for person in participant:
+        if participant[person]:
+            return person
