1345bba50505b59ba499bf6debb6b676c1d4e087
minimimin <139435516+minimimin@users.noreply.github.com>
2026-01-18 20:06:52 +0900

    [level 2] Title: 할인 행사, Time: 148.62 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+# from collections import defaultdict
+from collections import Counter
 
 def solution(want, number, discount):
     answer = 0
@@ -6,9 +7,9 @@ def solution(want, number, discount):
     for i in range(len(want)):
         wants[want[i]] = number[i]
     for idx in range(len(discount)-9):
-        check = defaultdict(int)
-        for cnt_idx in range(idx, idx+10):
-            check[discount[cnt_idx]] += 1
+        check = Counter(discount[idx:idx+10])
+        # for cnt_idx in range(idx, idx+10):
+        #     check[discount[cnt_idx]] += 1
         if wants == check:
             answer += 1
     return answer
