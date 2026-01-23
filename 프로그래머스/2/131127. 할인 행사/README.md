bc97f17382b8ebde6fd9211a4aaa0c8c4c0f1cf2
minimimin <139435516+minimimin@users.noreply.github.com>
2026-01-23 22:46:41 +0900

    [level 2] Title: 할인 행사, Time: 304.46 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 16.3 MB, 시간: 152.40 ms
+메모리: 16.4 MB, 시간: 304.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 18일 20:14:10
+2026년 01월 23일 22:46:51
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,10 +1,14 @@
-# from collections import defaultdict
-from collections import Counter
-
 def solution(want, number, discount):
     answer = 0
-    wants = { want[i]:number[i] for i in range(len(want))}
+    wants = {}
+    for i in range(len(want)):
+        wants[want[i]] = number[i]
     for idx in range(len(discount)-9):
-        if wants == Counter(discount[idx:idx+10]):
-            answer += 1
+        temp_wants = wants.copy()
+        for ten in range(10):
+            if discount[idx+ten] in temp_wants.keys() and temp_wants[discount[idx+ten]] >= 1:
+                temp_wants[discount[idx+ten]] -= 1
+        else:
+            if sum(temp_wants.values()) == 0:
+                answer += 1
     return answer
