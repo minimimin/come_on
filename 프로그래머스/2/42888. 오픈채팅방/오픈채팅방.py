da9281981b1e3dfeda0bd840edcde63a368a9353
minimimin <139435516+minimimin@users.noreply.github.com>
2026-01-23 23:50:44 +0900

    [level 2] Title: 오픈채팅방, Time: 125.60 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,23 +1,17 @@
-from collections import defaultdict
-
 def solution(record):
     answer = []
     # 중복 닉네임 허용, 유저 아이디로 구분 가능
     # O(N)
-    user = defaultdict(str)
-    logs = []
+    user = {}
     for com in record:
         command = com.split()
-        if command[0] == "Enter":
+        if command[0] in ["Enter", "Change"]:
             user[command[1]] = command[2]
-            logs.append(("Enter", command[1]))
-        elif command[0] == "Leave":
-            logs.append(("Leave", command[1]))
-        else:
-            user[command[1]] = command[2]
-    for log in logs:
+
+    for logs in record:
+        log = logs.split()
         if log[0] == "Enter":
             answer.append(user[log[1]]+"님이 들어왔습니다.")
-        else:
+        elif log[0] == "Leave":
             answer.append(user[log[1]]+"님이 나갔습니다.")
     return answer
