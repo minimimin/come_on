a97060f6bf1b30df647d130c6dc9fa0e82545263
minimimin <139435516+minimimin@users.noreply.github.com>
2026-02-16 17:58:39 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,10 @@
 from collections import defaultdict
 
 def solution(clothes):
-    # 각 행은 [이름, 종류]로 구성
-    # 종류별로 몇 개 있는지 카운트
-    # 종류별로 (1(안입기)+개수)만큼의 선택지 있음 => 각 선택지마다 곱하기
-    # 전부 다 안입을 수는 없음 = 1 빼기
-    count_clothes = defaultdict(int)
-    for _, t in clothes:
-        count_clothes[t] += 1
+    closet = defaultdict(int)
     answer = 1
-    for ty in count_clothes:
-        answer *= (count_clothes[ty]+1)
-    return answer - 1
+    for name, types in clothes:
+        closet[types] += 1
+    for clo_type in closet:
+        answer *= (closet[clo_type]+1)
+    return answer-1
