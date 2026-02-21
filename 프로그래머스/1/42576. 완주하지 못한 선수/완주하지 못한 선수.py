9e7a5d065294e730478f0e9ff45091e83eca4ba5
minimimin <139435516+minimimin@users.noreply.github.com>
2026-02-21 23:22:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.46 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,7 @@
 from collections import Counter
-
+    
 def solution(participant, completion):
-    participant = Counter(participant)
-    for person in completion:
-        participant[person] -= 1
-    for person in participant:
-        if participant[person]:
-            return person
+    com_dict = Counter(completion)
+    part_dict = Counter(participant)
+    for answer in (part_dict - com_dict):
+        return answer
