fc3ba9c1345d8dec9a2f727b23f0ed7b55f16eb4
minimimin <139435516+minimimin@users.noreply.github.com>
2026-03-06 00:48:30 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 1.68 ms, Memory: 9.63 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,32 +1,17 @@
-from collections import defaultdict
+from collections import Counter
+# from collections import defaultdict
 from itertools import combinations
 
 def solution(orders, course):
     answer = []
-    check = {}
-    for num in course:
-        check[num] = defaultdict(int)
-        
-    for idx in range(len(orders)-1):
-        std_set = set(orders[idx])
-        for next_i in range(idx+1,len(orders)):
-            exp_set = set(orders[next_i])
-            temp_course = []
-            for alp in exp_set:
-                if alp in std_set:
-                    temp_course.append(alp)
-            # 현재 상태에서 나온걸로 길이별 조합을 만들어야하는데,,
-            for lens in course:
-                johap = combinations(temp_course, lens)
-                for alp_jo in johap:
-                    sor_alp_jo = sorted(alp_jo)
-                    check[lens]["".join(sor_alp_jo)] += 1
-            
-    for num_cou in check:
-        if not check[num_cou]:
-            continue
-        max_cou = max(check[num_cou].values())
-        for dish in check[num_cou]:
-            if check[num_cou][dish] == max_cou:
-                answer.append(dish)
+    for course_len in course:
+        menu = []
+        for order in orders:
+            menu += combinations(sorted(order), course_len)
+        count_johap = Counter(menu)
+        if count_johap and max(count_johap.values()) > 1:
+            max_johap = max(count_johap.values())
+            for find in count_johap:
+                if count_johap[find] == max_johap:
+                    answer.append(''.join(find)) 
     return sorted(answer)
