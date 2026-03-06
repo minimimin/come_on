6cc4d9451969f1ecceac05b6340d46fc5be510cf
minimimin <139435516+minimimin@users.noreply.github.com>
2026-03-06 17:24:54 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 2.18 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,17 +1,22 @@
-from collections import Counter
-# from collections import defaultdict
+from collections import defaultdict
 from itertools import combinations
 
 def solution(orders, course):
     answer = []
-    for course_len in course:
-        menu = []
-        for order in orders:
-            menu += combinations(sorted(order), course_len)
-        count_johap = Counter(menu)
-        if count_johap and max(count_johap.values()) > 1:
-            max_johap = max(count_johap.values())
-            for find in count_johap:
-                if count_johap[find] == max_johap:
-                    answer.append(''.join(find)) 
-    return sorted(answer)
+    com_course = {x:defaultdict(int) for x in course}
+    for order in orders:
+        for num in course:
+            if len(order) >= num:
+                for johap in combinations(sorted(order), num):
+                    com_course[num][johap] += 1
+            else:
+                break
+    for num_course in com_course:
+        if com_course[num_course]:
+            choose_course = max(com_course[num_course].values())
+            if choose_course > 1:
+                for menu_johap in com_course[num_course]:
+                    if com_course[num_course][menu_johap] == choose_course:
+                        answer.append(''.join(menu_johap))
+    answer.sort()
+    return answer
