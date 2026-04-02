a36912404bf74a1129bb91e67b2099d16a9c168e
minimimin <139435516+minimimin@users.noreply.github.com>
2026-04-02 19:23:13 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.14 ms, Memory: 9.9 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,45 +1,22 @@
-from itertools import combinations
-from collections import Counter
-
 def solution(friends, gifts):
-    gift_jisu = {friend:0 for friend in friends}
-    # 각 사람별 선물지수 count하기
+    frd_dict = {friend:[0,{friend:0 for friend in friends}] for friend in friends}
+    next_mon_dict = {friend:0 for friend in friends}
     
-    relation = set(johap for johap in combinations(friends, 2))
-    # 나올 수 있는 관계 조합에서 선물 받을 사람 구하기
+    for gift in gifts:
+        give, take = gift.split(" ")
+        frd_dict[give][0] += 1
+        frd_dict[take][0] -= 1
+        frd_dict[give][1][take] += 1
     
-    gift_switch = Counter(gifts)
-    # 주고받은 횟수 count하기
-    
-    next_gift = {friend:0 for friend in friends}
-    # 각 사람별 다음달 받을 선물 count하기
-    
-    # 선물지수 구하기
-    for man in gifts:
-        check = man.split(" ")
-        gift_jisu[check[0]] += 1
-        gift_jisu[check[1]] -= 1
-
-    # 관계별 더 많이 받을 사람 구하기
-    for fri_jo in relation:
-        give = fri_jo[0] + " " + fri_jo[1]
-        rec = fri_jo[1] + " " + fri_jo[0]
-        if gift_switch[give] or gift_switch[rec]:
-            if gift_switch[give] > gift_switch[rec]:
-                next_gift[fri_jo[0]] += 1
-            elif gift_switch[give] < gift_switch[rec]:
-                next_gift[fri_jo[1]] += 1
-            else:
-                if gift_jisu[fri_jo[0]] > gift_jisu[fri_jo[1]]:
-                    next_gift[fri_jo[0]] += 1
-                elif gift_jisu[fri_jo[0]] < gift_jisu[fri_jo[1]]:
-                    next_gift[fri_jo[1]] += 1       
-
-        else:
-            if gift_jisu[fri_jo[0]] > gift_jisu[fri_jo[1]]:
-                next_gift[fri_jo[0]] += 1
-            elif gift_jisu[fri_jo[0]] < gift_jisu[fri_jo[1]]:
-                next_gift[fri_jo[1]] += 1
-        
-    # 제일 많이 받을 사람의 최대값 찾기
-    return max(next_gift.values())
+    for me in friends:
+        for friend in friends:
+            if me == friend:
+                continue
+            elif frd_dict[me][1][friend] > frd_dict[friend][1][me]:
+                next_mon_dict[me] += 1
+                continue
+            elif frd_dict[me][1][friend] == frd_dict[friend][1][me] and frd_dict[me][0] > frd_dict[friend][0]:
+                next_mon_dict[me] += 1
+                continue
+                
+    return max(next_mon_dict.values())
