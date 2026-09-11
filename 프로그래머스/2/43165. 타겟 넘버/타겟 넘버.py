b963970dcac675f4f74d4a1b8d45ea58b12475d1
minimimin <139435516+minimimin@users.noreply.github.com>
2026-09-11 20:55:17 +0900

    [level 2] Title: 타겟 넘버, Time: 185.80 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,10 +1,8 @@
 def solution(numbers, target):
     def dfs(now_idx, now_sum):
-        if now_idx == len(numbers) and now_sum == target:
+        if now_idx == len(numbers)-1 and now_sum == target:
             return 1
-        elif now_idx >= len(numbers):
+        elif now_idx >= len(numbers)-1:
             return 0
-            
-        return dfs(now_idx+1, now_sum + numbers[now_idx]) + dfs(now_idx+1, now_sum - numbers[now_idx])
-        
-    return dfs(1, 0 + numbers[0]) + dfs(1, 0 - numbers[0])
+        return dfs(now_idx+1, now_sum + numbers[now_idx+1]) + dfs(now_idx+1, now_sum - numbers[now_idx+1])
+    return dfs(0, numbers[0]) + dfs(0, -numbers[0])
